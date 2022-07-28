b9b78fde2212141106aac8ae448f44bd59e26479
Toader Daniel <hrpfase@gmail.com>
2022-07-28 22:09:20 +0300

    Make page optional

--- src/plugins/endpoints.ts
+++ src/plugins/endpoints.ts
@@ -1,6 +1,6 @@
 import {hostname} from '@/globals'
 
 export default {
-  candidates: (page: number) => `${hostname}/candidates${page ? `?_page=${page}` : ''}`,
+  candidates: (page?: number) => `${hostname}/candidates${page ? `?_page=${page}` : ''}`,
   candidate: (id: number) => `${hostname}/candidates/${id}`
 }
